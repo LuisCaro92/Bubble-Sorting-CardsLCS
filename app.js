e022dc1b05ba9a1451f7b9c5b965b060b5f55bc1
LuisCaro92 <luis.carosalgado1@gmail.com>
2022-12-10 16:59:40 +0000

    algo

---
+++ app.js
@@ -0,0 +1,12 @@
+let numeros=[ "A" , "2","3","4","5","6","7","8","9","J","Q","K"]
+let pinta=["♦", "♥", "♠", "♣"]
+
+let div=document.createElement("div")
+div.classList.add("card")
+document.body.append(div);
+div.getAttribute("numeros");
+
+let pinta1=document.createElement(div);
+pinta1.classList.add('icono1')
+div.appendChild(pinta1);
+fi
