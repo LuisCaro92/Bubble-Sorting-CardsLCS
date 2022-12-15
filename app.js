830b41c2b607e135ed955b7fcfe6290a4512eae9
LuisCaro92 <luis.carosalgado1@gmail.com>
2022-12-15 22:55:46 +0000

    vamos bien

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
 let numeros=[ "A" , "2","3","4","5","6","7","8","9","J","Q","K"]
-let pinta=["♦", "♥", "♠", "♣"]
+let pintas=["♦", "♥", "♠", "♣"]
 
-let div=document.createElement("div")
-div.classList.add("card")
-document.body.append(div);
-div.getAttribute("numeros");
+let div=document.getElementById("card")
+div.classList.add("id","card")
 
-let pinta1=document.createElement(div);
-pinta1.classList.add('icono1')
-div.appendChild(pinta1);
-fi
+
+
+function cartaRandom(){
+
+
+  let numeroaleatorio = Math.floor(Math.random() * numeros.length);
+  let iconoaleatorio = Math.floor(Math.random() * pintas.length);
+
+  document.getElementById("pintaup").innerHTML = pintas[iconoaleatorio];
+  document.getElementById("numero").innerHTML = numeros[numeroaleatorio];
+  document.getElementById("pintadown").innerHTML = pintas[iconoaleatorio];
+
+
+  if (iconoaleatorio == 0 || iconoaleatorio == 1) {
+    document.getElementById("pintaup").style.color = "red";
+    document.getElementById("pintadown").style.color = "red";
+    document.getElementById("numero").style.color="red"
+  }
+}
+
+const randomsuit=pintas[cartaRandom()];
+
+/* funcion para el sort*/
+
+
+
+ 
